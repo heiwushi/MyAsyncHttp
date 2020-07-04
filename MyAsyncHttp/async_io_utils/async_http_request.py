d2a658e50ac9033818b7e2a55d889dc8eb5a1377
heiwushi <447648965@qq.com>
2020-07-04 22:07:24 +0800

    do some work about async_http_request

--- MyAsyncHttp/async_io_utils/async_http_request.py
+++ MyAsyncHttp/async_io_utils/async_http_request.py
@@ -1,21 +1,53 @@
 from ..loop import get_event_loop
 import socket
 from .. import container
+from .. http_utils import parse_http_response
 import select
 import queue
-
+from collections import defaultdict
+import traceback as tb
+import logging
 loop = get_event_loop()
+
+
+DEFAULT_HEADER = {
+    "CONNECTION": "close" #暂时不支持keep-alive
+}
+
+HTTP_VERSION = "HTTP/1.1"
+
+
+fd_callback_dict = defaultdict(dict)
 
 
 def _epollin_event_callback(fd, event):
     '''
-    有socket数据读入,即收到了http服务端发来的数据
+    有socket数据读入,即收到了http客户端发来的数据
     :param fd:
     :param event:
     :return:
     '''
-    pass
 
+    event_socket, event_type = container.get_fd_infos(fd)
+    recv_data = event_socket.recv(1024)
+    if len(recv_data) == 0:
+        print("连接断开")
+        loop.unregister(fd)
+        event_socket.close()
+        container.del_fd_infos(fd)
+    else:
+        print("可读:")
+        try:
+            fd_callback_dict[fd]["success"](recv_data)
+            #response = parse_http_response(recv_data.decode('utf-8'))
+
+            #print(response)
+        except Exception as e:
+            error_stack_msg = tb.format_exc()
+            logging.error(error_stack_msg)
+        finally:
+            loop.unregister(fd)
+            container.del_fd_infos(fd)
 
 def _epollout_event_callback(fd, event):
     '''
@@ -28,25 +60,56 @@ def _epollout_event_callback(fd, event):
     try:
         request_data = container.write_buffer[fd].get_nowait()
         event_socket.send(request_data)
+        loop.unregister(fd)
+        loop.register(fd, select.EPOLLIN, _epollin_event_callback)
+
     except queue.Empty as e:
         event_socket.close()
         loop.unregister(fd)
         container.del_fd_infos(fd)
 
 
-def get( url, success_callback, error_callback):
-    url=url.replace("http://", "")
-    address_port = url.split('/')[0].split(":")
-    if len(address_port)==1:
-        address = address_port
-        port = 80
-    else:
-        address, port = address_port
+def build_http_header_line(method, endpoint, header):
+    request_msg = ""
+    request_msg += method + " " + endpoint + " " + HTTP_VERSION + "\r\n"
+    for head_name in header:
+        request_msg += head_name+":"+header[head_name]+"\r\n"
+    request_msg += "\r\n"
+    return request_msg
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.setblocking(False)
-    container.write_buffer[client_socket.fileno()].put_nowait("")
-    container.add_fd_infos(client_socket.fileno(), client_socket, "socket")
-    server_address = (address, port)
-    client_socket.connect(server_address)
-    loop.register(client_socket.fileno(), select.EPOLLOUT, _epollout_event_callback)
+
+def request(method: str, url: str, header=None, success_callback=None, error_callback=None):
+    try:
+        assert url.startswith("http://")#暂时只支持http协议
+        method = method.upper()
+        url=url.replace("http://", "")
+        url_splits = url.split('/')
+        address_port = url_splits[0].split(":")
+        if len(address_port) == 1:
+            address = address_port[0]
+            port = 80
+        else:
+            address, port = address_port
+
+        if len(url_splits) > 1:
+            end_point = "/" + "/".join(url_splits[1:])
+        header=dict(header)
+        header.update(DEFAULT_HEADER)
+        header["HOST"]=address
+        request_msg = build_http_header_line(method, end_point, header)
+        request_bytes = request_msg.encode('ascii')
+        print(request_msg)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        fd = client_socket.fileno()
+        fd_callback_dict[fd]["success"] = success_callback
+        fd_callback_dict[fd]["error"] = error_callback
+        container.add_fd_infos(fd, client_socket, "socket")
+        container.write_buffer[fd].put_nowait(request_bytes)
+        server_address = (address, port)
+        client_socket.connect(server_address)
+        client_socket.setblocking(False)
+        loop.register(fd, select.EPOLLOUT, _epollout_event_callback)
+        loop.start()
+    except Exception as e:
+        error_callback(e, tb.format_exc())
+
--- MyAsyncHttp/http_utils.py
+++ MyAsyncHttp/http_utils.py
@@ -18,11 +18,11 @@ def parse_http_request(recv_data):
     header_finish = False
     data = []
     params = {}
-    for line in request_lines:
+    for line in request_lines[1:]:
         print(line)
         if line != "":
             if header_finish:
-                header_name, header_value = line.split(" ")
+                header_name, header_value = line.split(":")
                 headers[header_name] = header_value
             else:
                 data.append(line)
@@ -37,3 +37,27 @@ def parse_http_request(recv_data):
                 param, value = param_value.split('=')
                 params[param] = value
     return Request(end_point, method, http_version, headers, data, params)
+
+
+def parse_http_response(recv_data):
+    """为一个客户端服务"""
+    request_lines = recv_data.splitlines()
+    http_version, status_code, status_info = request_lines[0].split(' ')
+    status_code = int(status_code)
+
+
+    headers = {}
+    header_finish = False
+    data = []
+    params = {}
+    for line in request_lines[1:]:
+        print(line)
+        if line != "":
+            if header_finish:
+                header_name, header_value = line.split(" ")
+                headers[header_name] = header_value
+            else:
+                data.append(line)
+        else:
+            header_finish = True
+    return data
